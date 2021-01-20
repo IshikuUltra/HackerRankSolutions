863d1561aa8538f067888983325f2cf8aed15d1f
Sam Goodnight <mason.rad@tutanota.com>
2021-01-20 15:26:29 -0600

    update

--- nds.js
+++ nds.js
@@ -5,24 +5,25 @@ function nds (s, k) {
     if(k == 1 || k == 0) {
         return 1;
     }
-    //take remainders of all the ints in s
+
+    let frequency = {}; 
     let r = [];
     // how to measure forEach() on arrays > 80,000
     // should i combine the e%k into the r[n] + r[n+i] -> (n%k) + (n+i%k) == k ? Better performance?
-    // track frequency here since we already looping?
-    s.forEach((e) => {r[r.length] = (e % k); });
-    // now we need to check if r[n] + r[n+i] is k
+    s.forEach((e) => {let eModK = (e%k); r[r.length] = (eModK); eModK in frequency ? frequency[eModK]+=1 : frequency[eModK] = 0;});
     //USE COUNTERS INSTEAD OF STORING THE NUMBERS:
     for(let i=0; i < r.length; i++) {
         for(let j=1; j < r.length; j++) {
-            if(s[i] + s[j] === k) {
+            if(r[i] + r[j] === k) { 
                 //keep the one with the higher frquency 
+                frequency[r[i]] > frequency[r[j]] ? r.splice(r.indexOf(r[i]),1) : r[i] > r[j] ? r.splice(r.indexOf(r[j]),1) : r.splice(r.indexOf(r[i]),1);
             }
         }
     }
-    return r;
+    console.log(r)
+    return r.length; // r = [6,9,2,2,1,8,0]
 }
 
-const arr = [2375782, 21836421, 2139842193, 2138723, 23816, 21836219, 2948784 ,43864923 ,283648327, 23874673];
+const arr = [2375782, 21836421, 2139842193, 2138723, 23816, 21836219, 2948784 ,43864923 ,283648327, 23874673]; 
 const k = 13;
 console.log(nds(arr, k)); // return 7
